c9041fcd17063568394a7141af991ddeea7aae0a
michael-hampton <michaelhamptondesign@yahoo.com>
2021-04-02 10:04:32 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <h1>Hangman</h1>
-            <p>Find the hidden word - Enter a letter</p>
+            <h1>Hangman - Countries of the world</h1>
+            <p>Can you guess the name of the country - Enter a letter</p>
         </>
     )
 }
